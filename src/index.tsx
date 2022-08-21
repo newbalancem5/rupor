4e7b7ef3d68de14dbc94244e448b63a127f84e6c
newbalancem5 <newbalancem5@gmail.com>
2022-08-21 19:37:11 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Welcome from './page/welcome';
+import './style/global.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
